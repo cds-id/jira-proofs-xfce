b60b40b6a0c550d8b4869af0a4769791a7fcb1a5
Indra Gunanda <indra.gunanda@gmail.com>
2026-03-15 14:01:49 +0700

    feat: add Win32 resource definitions (dialogs, wizard pages)

---
+++ lib/ui-win32/resource.h
@@ -0,0 +1,45 @@
+#ifndef __WIN_RESOURCE_H__
+#define __WIN_RESOURCE_H__
+
+/* Main capture dialog */
+#define IDD_MAIN_DIALOG         100
+#define IDC_RADIO_FULLSCREEN    1001
+#define IDC_RADIO_WINDOW        1002
+#define IDC_RADIO_REGION        1003
+#define IDC_CHECK_MOUSE         1004
+#define IDC_SPIN_DELAY          1005
+#define IDC_COMBO_ACTION        1006
+#define IDC_BTN_CAPTURE         1007
+#define IDC_BTN_PREFERENCES     1008
+#define IDC_BTN_CLOUD_SETUP     1009
+#define IDC_GROUP_REGION        1010
+#define IDC_GROUP_OPTIONS       1011
+#define IDC_GROUP_ACTION        1012
+#define IDC_LABEL_DELAY         1013
+
+/* Wizard pages */
+#define IDD_WIZARD_WELCOME      200
+#define IDD_WIZARD_R2           201
+#define IDD_WIZARD_JIRA         202
+#define IDD_WIZARD_DONE         203
+
+#define IDC_WIZARD_LABEL        2001
+#define IDC_R2_ACCOUNT_ID       2010
+#define IDC_R2_ACCESS_KEY       2011
+#define IDC_R2_SECRET_KEY       2012
+#define IDC_R2_BUCKET           2013
+#define IDC_R2_PUBLIC_URL       2014
+#define IDC_R2_TEST_BTN         2015
+#define IDC_R2_TEST_RESULT      2016
+#define IDC_JIRA_BASE_URL       2020
+#define IDC_JIRA_EMAIL          2021
+#define IDC_JIRA_API_TOKEN      2022
+#define IDC_JIRA_PROJECT        2023
+#define IDC_JIRA_TEST_BTN       2024
+#define IDC_JIRA_TEST_RESULT    2025
+#define IDC_DONE_SUMMARY        2030
+
+/* App icon */
+#define IDI_APP_ICON            300
+
+#endif
